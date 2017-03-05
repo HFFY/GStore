456d552e2e340c8d04eaea0699602090326c9801
Sergio <sbellott@hotmail.com>
2017-03-05 16:36:43 -0400

    Agregando la clase Datos "Base de datos SQL"

--- app/src/main/java/com/example/hffy/gstore/Datos.java
+++ app/src/main/java/com/example/hffy/gstore/Datos.java
@@ -1,13 +1,58 @@
 package com.example.hffy.gstore;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
 /**
  * Created by HFFY on 2/3/2017.
  */
 
-public class Datos {
+public class Datos extends SQLiteOpenHelper {
 
-    // TODO: crear una forma de almacenar datos enviados por Json
-    String Datos;
+    public static abstract class DatosTabla implements BaseColumns{
+        public static final String NOMBRE_TABLA = "Datos de Usuario";
+        public static final String CAMPO_USUARIO = "Usuario";
+        public static final String CAMPO_CONTRASEÑA = "Contraseña";
+        public static final String CAMPO_EMAIL = "Email";
+        public static final String CAMPO_CELULAR = "Celular";
+        public static final String CAMPO_CODIGO = "Codigo";
+
+        private static final String TEXT_TYPE = " TEXTO";
+        private static final String COMMA_SEP = ",";
+        private static final String CREAR_TABLA_1 = "Create table "
+                + DatosTabla.NOMBRE_TABLA + " ("+
+                DatosTabla.CAMPO_USUARIO + TEXT_TYPE + COMMA_SEP +
+                DatosTabla.CAMPO_CONTRASEÑA + TEXT_TYPE + COMMA_SEP +
+                DatosTabla.CAMPO_EMAIL + TEXT_TYPE + COMMA_SEP +
+                DatosTabla.CAMPO_CELULAR + TEXT_TYPE + COMMA_SEP +
+                DatosTabla.CAMPO_CODIGO + TEXT_TYPE + " )";
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + DatosTabla.NOMBRE_TABLA;
+
+    }
+
+    public Datos(Context context){
+        super(context, DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    public static final int DATABASE_VERSION= 1;
+    public static final String DATABASE_NAME = "Datos.db";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(DatosTabla.CREAR_TABLA_1);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DatosTabla.SQL_DELETE_ENTRIES);
+        onCreate(db);
+
+    }
 
 
     public boolean usuario(String u, String p){
@@ -18,7 +63,6 @@ public class Datos {
 
     public boolean crear(String uss,String pass,String email,String celular,String codigo){
         //TODO Almacenar un nuevo Usuario con todos sus datos
-
         return true;
     }
 
@@ -26,11 +70,9 @@ public class Datos {
     private static Datos instance = null;
     public static Datos getInstance() {
         if (instance == null) {
-            instance = new Datos();
+            instance = new Datos(null);
         }
         return instance;
     }
-
-    private Datos(){};
 
 }
