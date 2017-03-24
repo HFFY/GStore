ce07cd7d4773d5c37e71998d2c1b9526058f02f3
MonoMatheus <monkeymon1313@gmail.com>
2017-03-23 21:03:18 -0400

    Descripciones,imagenes,urls faltantes

--- app/src/main/java/com/example/hffy/gstore/Apple.java
+++ app/src/main/java/com/example/hffy/gstore/Apple.java
@@ -35,7 +35,7 @@ public class Apple extends AppCompatActivity {
         sliderLayout=(SliderLayout)findViewById(R.id.sliderApple);
 
         DefaultSliderView slider=new DefaultSliderView(context);
-        slider.image(R.drawable.appleiphone);
+        slider.image(R.drawable.iphonegrande);
         slider.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
             @Override
             public void onSliderClick(BaseSliderView baseSliderView) {
@@ -45,7 +45,7 @@ public class Apple extends AppCompatActivity {
         sliderLayout.addSlider(slider);
 
         DefaultSliderView slider1=new DefaultSliderView(context);
-        slider1.image(R.drawable.appleimac);
+        slider1.image(R.drawable.imac1grande);
         slider1.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
             @Override
             public void onSliderClick(BaseSliderView baseSliderView) {
@@ -55,7 +55,7 @@ public class Apple extends AppCompatActivity {
         sliderLayout.addSlider(slider1);
 
         DefaultSliderView slider2=new DefaultSliderView(context);
-        slider2.image(R.drawable.applemacbookpro);
+        slider2.image(R.drawable.macbookprogrande);
         slider2.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
             @Override
             public void onSliderClick(BaseSliderView baseSliderView) {
@@ -65,7 +65,7 @@ public class Apple extends AppCompatActivity {
         sliderLayout.addSlider(slider2);
 
         DefaultSliderView slider3=new DefaultSliderView(context);
-        slider3.image(R.drawable.applemabook);
+        slider3.image(R.drawable.macbookgrande);
         slider3.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
             @Override
             public void onSliderClick(BaseSliderView baseSliderView) {
@@ -75,7 +75,7 @@ public class Apple extends AppCompatActivity {
         sliderLayout.addSlider(slider3);
 
         DefaultSliderView slider4=new DefaultSliderView(context);
-        slider4.image(R.drawable.appleipad);
+        slider4.image(R.drawable.ipadminigrande);
         slider4.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
             @Override
             public void onSliderClick(BaseSliderView baseSliderView) {
@@ -85,7 +85,7 @@ public class Apple extends AppCompatActivity {
         sliderLayout.addSlider(slider4);
 
         DefaultSliderView slider5=new DefaultSliderView(context);
-        slider5.image(R.drawable.appleiphone);
+        slider5.image(R.drawable.iphone7grande);
         slider5.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
             @Override
             public void onSliderClick(BaseSliderView baseSliderView) {
@@ -128,12 +128,12 @@ public class Apple extends AppCompatActivity {
          */
 
 
-        items.add(new Item(1, 123,"Iphone", "lo ultimo en smart phones de la linea iphone con sistema operativo IO's en su ultima version", R.drawable.appleiphone,"DTh-I70X_YU", R.drawable.appleiphone,"Descripcion con unboxing del dispositivo Iphone en general en el video (click en la imgen) "));
-        items.add(new Item(2, 123,"Imac", "Computadora de escritorio de la linea Apple bastante comoda y actual para el uso en casa o oficinas", R.drawable.appleimac,"yZp1heY3vCY", R.drawable.appleimac," Descripcion con unboxing del dispositivo IMac en el video (click en la imgen)"));
-        items.add(new Item(3, 123,"MacBookpro", "Computadora portátil Apple dirigida especificamente a aquellos que desean un mejor rendimiento y capacidad de procesamiento", R.drawable.applemacbookpro,"2GftT2og8Uc", R.drawable.applemacbookpro," Descripcion con unboxing de la MaxbookPro en el video (click en la imgen)"));
-        items.add(new Item(4, 123,"MacBook", "Computadora portátil de uso mas casual aunque sin dejar de ser bastante util y moderna", R.drawable.applemabook,"3OIftOhgTyw", R.drawable.applemabook,"Descripcion con unboxing de la Macbook en el video (click en la imgen)"));
-        items.add(new Item(5, 123,"IPadMini", "Computadora de estilo tableta de tamaño mediano con sistema operativo IO's de uso mas familiar o de trabajo", R.drawable.appleipad,"VyvfDDSOrao", R.drawable.appleipad,"Descripcion con unboxing del IpadMini en el video (click en la imgen) "));
-        items.add(new Item(6, 123,"Iphone 7 Plus", "Celular de Apple de última generación bastante comodo y tambien de multiples herramientas  ", R.drawable.appleiphone,"4-4Jz6Lr014", R.drawable.appleiphone,"Descripcion con unboxing del iphone 7 en el video (click en la imgen) "));
+        items.add(new Item(1, 123,"Iphone", "lo ultimo en smart phones de la linea iphone con sistema operativo IO's en su ultima version", R.drawable.appleiphone,"DTh-I70X_YU", R.drawable.iphonegrande,"Descripcion con unboxing del dispositivo Iphone en general en el video (click en la imgen) "));
+        items.add(new Item(2, 123,"Imac", "Computadora de escritorio de la linea Apple bastante comoda y actual para el uso en casa o oficinas", R.drawable.appleimac,"yZp1heY3vCY", R.drawable.imac1grande," Descripcion con unboxing del dispositivo IMac en el video (click en la imgen)"));
+        items.add(new Item(3, 123,"MacBookpro", "Computadora portátil Apple dirigida especificamente a aquellos que desean un mejor rendimiento y capacidad de procesamiento", R.drawable.applemacbookpro,"2GftT2og8Uc", R.macbookprogrande," Descripcion con unboxing de la MaxbookPro en el video (click en la imgen)"));
+        items.add(new Item(4, 123,"MacBook", "Computadora portátil de uso mas casual aunque sin dejar de ser bastante util y moderna", R.drawable.applemabook,"3OIftOhgTyw", R.drawable.macbookgrande,"Descripcion con unboxing de la Macbook en el video (click en la imgen)"));
+        items.add(new Item(5, 123,"IPadMini", "Computadora de estilo tableta de tamaño mediano con sistema operativo IO's de uso mas familiar o de trabajo", R.drawable.appleipad,"VyvfDDSOrao", R.drawable.ipadminigrande,"Descripcion con unboxing del IpadMini en el video (click en la imgen) "));
+        items.add(new Item(6, 123,"Iphone 7 Plus", "Celular de Apple de última generación bastante comodo y tambien  de multiples herramientas  ", R.drawable.appleiphone,"4-4Jz6Lr014", R.drawable.iphone7grande,"Descripcion con unboxing del iphone 7 en el video (click en la imgen) "));
         AdaptadorItem adaptador=new AdaptadorItem(Apple.this, items);
         lista.setAdapter(adaptador);
 
